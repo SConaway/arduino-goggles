b0635a7b909b6609198bdd922e9a9ba44c3312b0
Steven Conaway <Steven.conaway@icloud.com>
2019-01-11 20:37:39 -0800

    Fix colors.h

--- colors.h
+++ colors.h
@@ -4,11 +4,13 @@
     #define NUMCOLORS 2
     #define COLOR1 (uint32_t) 0xFF0000
     #define COLOR2 (uint32_t) 0x00FF00
-  #elseif defined(AMERICAN)
-    #define NUMCOLORS 3
-    #define COLOR1 (uint32_t) 0xFF0000
-    #define COLOR2 (uint32_t) 0xFFFFFF
-    #define COLOR3 (uint32_t) 0x0000FF
+  #else
+    #if defined(AMERICAN)
+      #define NUMCOLORS 3
+      #define COLOR1 (uint32_t) 0xFF0000
+      #define COLOR2 (uint32_t) 0xFFFFFF
+      #define COLOR3 (uint32_t) 0x0000FF
+    #endif
   #endif
 #endif
 
