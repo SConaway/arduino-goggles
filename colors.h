ebf621d53132c433d7ad31946c31f3b3f6dbcda3
Steven Conaway <steven.conaway@icloud.com>
2019-11-24 12:03:00 -0800

    Halloween only has two colors

--- colors.h
+++ colors.h
@@ -15,17 +15,12 @@
     #else
       #if defined(HALLOWEEN)
         #warning "Halloween mode enabled"
-        #define NUMCOLORS 3
-        #define COLOR1 (uint32_t) 0x000000   // Black
-        #define COLOR2 (uint32_t) 0xFFA500   // Orange
-        #define COLOR3 (uint32_t) 0xFFFFFF   // WHITE
+        #define NUMCOLORS 2
+        #define COLOR1 (uint32_t) 0xFF5500   // Orange
+        #define COLOR2 (uint32_t) 0x7F7F7F   // WHITE
+    #endif
     #endif
   #endif
 #else
 #warning "Custom mode enabled"
 #endif
-
-// Custom mode
-//#define CUSTOM
-// Use bytes to express color
-//#define COLOR1
