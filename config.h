674e283862b0fe06b89428ce17ec6a99e72186f5
Steven Conaway <steven.conaway@icloud.com>
2019-11-24 12:03:13 -0800

    Configure Custom a bit

--- config.h
+++ config.h
@@ -2,14 +2,18 @@
 //#define CHRISTMAS
 
 // American mode
-//#define AMERICAN
+#define AMERICAN
 
 // Halloween mode
-#define HALLOWEEN
+//#define HALLOWEEN
 
 // Custom mode
 //#define CUSTOM
 // Use bytes to express color
-//#define COLOR1
+#define NUMCOLORS 4
+#define COLOR1 (uint32_t) 0xFF0000   // RED
+#define COLOR2 (uint32_t) 0x0F0F0F   // WHITE
+#define COLOR3 (uint32_t) 0x00FF00   // GREEN
+#define COLOR4 (uint32_t) 0x0000FF   // BLUE
 
-#define DEBUG
+//#define DEBUG
